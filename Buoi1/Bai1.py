c8e98ba29e0b93eb6edfd3a34a5491a53872ce3e
TranTrongDatt <dat.tt0278@sinhvien.hoasen.edu.vn>
2025-05-14 11:25:33 +0700

    [add] Buoi1

---
+++ Buoi1/Bai1.py
@@ -0,0 +1,6 @@
+#Nhập tên và tuổi từ người dùng
+ten = input("Vui lòng nhập tên của bạn: ")
+tuoi = input("Vui lòng nhập tuổi của bạn: ")
+
+#In thông điệp chào mừng với thông tin vừa nhập
+print("\n ~~~ Chào mừng", ten, "! Bạn", tuoi, "tuổi ~~~\n")
