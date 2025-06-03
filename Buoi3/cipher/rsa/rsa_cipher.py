2d5f6097f79be48c957a33e940f4941b4908d9d8
BuiBaoHan <dat.tt00278@sinhvien.hoasen.edu.vn>
2025-06-04 04:16:26 +0700

    Add files via upload

---
+++ Buoi3/cipher/rsa/rsa_cipher.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from ui.rsa import Ui_MainWindow
+import requests
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_gen_keys.clicked.connect(self.call_api_gen_keys)
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+        self.ui.btn_sign.clicked.connect(self.call_api_sign)
+        self.ui.btn_verify.clicked.connect(self.call_api_verify)
+    
+    def call_api_gen_keys(self):
+        url = "http://127.0.0.1:5000/api/rsa/generate_keys"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText(data["message"])
+                msg.exec_()
+            else:
+                print("Error while calling API")
+        except requests.exceptions.RequestException as e:
+            print("Error: %s" % e.message)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/rsa/encrypt"
+        payload = {
+            "message": self.ui.txt_plain_text.toPlainText(),
+            "key_type": "public"
+        }
+        try:
