eed0421e274d0a0b8676a273cff59542b3773aa0
BuiBaoHan <dat.tt00278@sinhvien.hoasen.edu.vn>
2025-06-18 00:23:47 +0700

    Delete Buoi1 directory

--- Buoi1/Bai3.py
+++
@@ -1,8 +0,0 @@
-#Nhập số từ người dùng
-so = int(input("\nVui lòng nhập 1 số nguyên: "))
-
-#Kiểm tra xem số đó có phải số chẵn hay không
-if so % 2 == 0:
-    print(so, "LÀ SỐ CHẴN !!!")
-else:
-    print(so, "KHÔNG PHẢI LÀ SỐ CHẴN !!!")
